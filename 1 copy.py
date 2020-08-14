9e30ac39cd25cfc029424070bc58b18fe7942e92
Игорь Дмитренко <idmytrenko@romstal.local>
2020-08-14 17:49:18 +0300

    ап

--- 1 copy.py
+++ 1 copy.py
@@ -8,52 +8,57 @@ import requests as rq
 
 hdr = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
-url = ('https://finviz.com/screener.ashx?v=111')
+url = ('https://finviz.com/screener.ashx?v=111&r=1')
 
-# def getTitle(url):
-#     try:
-#         html = urllib.request.Request(url,headers=hdr)
-#         responce = urlopen(html)
-#     except ur.HTTPError:  # если ошибка то выводим сообщение
-#         return 'Http Error or Access denite'
-#     except ur.URLError:
-#         return "Bad Link"
-#     try:
-#         html = urllib.request.Request(url,headers=hdr)
-#         responce = urlopen(html)
-#         bsObj = bs(responce.read(), features='lxml')
-#         title = bsObj.body.tr
-#     except AttributeError:  # если нету атрибута то выводим сообщение
-#         return 'Title could not be found'
-#     title = bs(responce.read(), features='lxml')
-#     return title
-# #getTitle(url)
-
-#soup = bs(getTitle(url).read(), features="lxml")
-a = 21
-for i in range(377):
-    a = a + 20
-    html = urllib.request.Request(url,headers=hdr)
-    responce = urlopen(html)
-    soup = bs(responce.read(), features='lxml')
-
-    #проверка выводимого текста
-    # with open("output1.html", "w",encoding='utf-8') as file:
-    #     file.write(str(soup))
-    #print(soup.get_text().encode('utf-8'))
-    mydivs = soup.find("table", {"bgcolor": "#d3d3d3"})
-    tables_row = mydivs.find_all('tr')
-    res = []
-    res1 = []
-    for tr in tables_row:
-        td = tr.find_all('td')
-        row = [tr.text for tr in td if tr.text]
-        if row:
-            res.append(row)
+def getTitle(url):
+    try:
+        html = urllib.request.Request(url,headers=hdr)
+        html = urlopen(html)
+    except ur.HTTPError:  # если ошибка то выводим сообщение
+        return 'Http Error or Access denite'
+    except ur.URLError:
+        return "Bad Link"
+        
+print(getTitle(url))
 
 
-    df = pd.DataFrame(res)
-    df.to_csv('1.csv', mode='a', header=False)
-    print(df)
-    url1 = 'https://finviz.com/screener.ashx?v=111&r='
-    url = url1 + str(a)
+
+
+def finviz_all_stocks(url, output=False, output_in_file_html=False):
+    """собирает все акции с финвиза"""
+    if getTitle(url) != None:
+        return getTitle(url)
+    a = 20
+    for i in range(375):
+        
+        html = urllib.request.Request(url, headers=hdr)
+        responce = urlopen(html)
+        soup = bs(responce.read(), features='lxml')
+        if output_in_file_html == True:
+            # проверка выводимого текста
+            with open("output_test.html", "w", encoding='utf-8') as file:
+                file.write(str(soup))
+                #print(soup.get_text().encode('utf-8'))
+            return 
+        mydivs = soup.find("table", {"bgcolor": "#d3d3d3"})
+        tables_row = mydivs.find_all('tr')
+        res = []
+        for tr in tables_row[1:]:
+            td = tr.find_all('td')
+            row = [tr.text for tr in td[:-3] if tr.text]
+            if row:
+                res.append(row)
+
+        df = pd.DataFrame(res)
+
+        df.to_csv('2.csv', mode='a', header=False)
+     
+        if output == True:
+            print(df)
+        url1 = 'https://finviz.com/screener.ashx?v=111&r='
+        url = url1 + str(a)
+        
+        a = a + 20
+
+
+finviz_all_stocks(url,output=True)
--- test.py
+++ test.py
@@ -1,17 +1,60 @@
-# from urllib.request import urlopen
-# import urllib.request
-# import bs4
-# hdr = {
-#     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
-# url = ('https://finviz.com/screener.ashx?v=211')
+from bs4 import BeautifulSoup as bs
+import prettify
+import pandas as pd
+from urllib.request import urlopen
+import urllib.request
+import urllib.error as ur  # import HTTPError
+from urllib import robotparser
+import requests as rq
+from selenium import webdriver
+import random
+hdr = {
+    'Host': 'www.kicksusa.com',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/72.0.3626.121 Safari/537.36',
+    'DNT': '1',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'ru,en-US;q=0.9,en;q=0.8,tr;q=0.7',
+    'Cookie': 'visid_incap_...=put here your visid_incap_ value; incap_ses_...=put here your incap_ses_ value',}
+url = 'https://www.streetinsider.com/ec_earnings.php?day=2020-08-13'
 # html = urllib.request.Request(url,headers=hdr)
 # responce = urlopen(html)
 # print(responce.read())
-url = ('https://finviz.com/screener.ashx?v=111')
-a = 21
-for i in range(4):
-    
-    a += 20
-    url1 = 'https://finviz.com/screener.ashx?v=111&r='
-    url = url1 + str(a)
-    print(url)
+
+url = "https://www.streetinsider.com/ec_earnings.php?day=2020-08-13"
+UAS = ("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1", 
+       "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
+       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0",
+       "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+       "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+       )
+
+ua = UAS[random.randrange(len(UAS))]
+
+hdr = {'user-agent': ua}
+r = rq.get(url, headers=hdr)
+print(r)
+
+
+html = urllib.request.Request(url, headers=hdr)
+responce = urlopen(html)
+soup = bs(responce.read(), features='lxml')
+with open("output_test.html", "w", encoding='utf-8') as file:
+    file.write(str(soup))
+mydivs = soup.find("table", {"class": "earning_hystory"})
+tables_row = mydivs.find('tr')
+res = []
+for tr in tables_row:
+    td = tr.find_all('td')
+    row = [tr.text for tr in td if tr.text]
+    if row:
+        res.append(row)
+
+df = pd.DataFrame(res)
+
+df.to_csv('2.csv', mode='a', header=False)
