f5d04ff50c507060c2f68bfe5307ff0b5ea26f2c
Игорь Дмитренко <idmytrenko@romstal.local>
2020-08-18 16:17:01 +0300

    fgh

---
+++ main.py
@@ -0,0 +1,15 @@
+import Screener_parser
+import Earning_TradingView
+
+
+print('1) Finviz screener\n2) TradingView earning\n3) All' )
+
+a = int(input())
+if a == 1:
+    Screener_parser.finviz_all_stocks()
+elif a == 2: 
+    Earning_TradingView.tradingview_earning()
+elif a ==3:
+    Earning_TradingView.tradingview_earning()
+    Screener_parser.finviz_all_stocks()
+    
